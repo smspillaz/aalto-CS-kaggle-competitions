7804b29457cff12ef2e7f44d4b71bc1def7104cc
Sam Spilsbury <smspillaz@gmail.com>
2019-01-23 08:54:40 +0200

    report: Show logistic loss in classification report

--- two-sigma-rental-interest/utils/report.py
+++ two-sigma-rental-interest/utils/report.py
@@ -1,10 +1,15 @@
 import numpy as np
 import pandas as pd
 import seaborn as sn
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, log_loss
 from IPython.display import display
 
-def generate_classification_report_from_preds(preds, orig_data, labels, classes, columns=None):
+def generate_classification_report_from_preds(preds,
+                                              probabilities,
+                                              orig_data,
+                                              labels,
+                                              classes,
+                                              columns=None):
     report_columns = (columns or ['description', 'price', 'bedrooms', 'bathrooms', 'photos_count'])
     for outer in classes:
         label_indices = frozenset([i for i, e in enumerate(labels) if e == outer])
@@ -20,8 +25,14 @@ def generate_classification_report_from_preds(preds, orig_data, labels, classes,
     display(sn.heatmap(confusion_matrix(preds, labels),
                        annot=True))
     print(classification_report(preds, labels))
+    print("Logistic loss {:.5f}".format(log_loss(labels, probabilities, labels=classes)))
 
 
 def generate_classification_report(model, orig_data, data, labels, classes):
     preds = model.predict(data)
-    return generate_classification_report_from_preds(preds, orig_data, labels, classes)
+    probabilities = model.predict_proba(data)
+    return generate_classification_report_from_preds(preds,
+                                                     probabilities,
+                                                     orig_data,
+                                                     labels,
+                                                     classes)
