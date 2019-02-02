a8dd2c71048eb119e4efbb4644b27dc2eece72cd
Sam Spilsbury <smspillaz@gmail.com>
2019-02-03 00:09:59 +0200

    ulmfit: Move most of the ulmfit training utils to be internal
    The caller doesn't need to know the details.

--- two-sigma-rental-interest/utils/language_models/ulmfit.py
+++ two-sigma-rental-interest/utils/language_models/ulmfit.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 from fastai.text import *
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import train_test_split
 from utils.dataframe import random_oversample_dataframe
+from utils.report import generate_classification_report_from_preds
 
 
 def load_ulmfit_classifier_with_transfer_learning_from_data_frame(training_dataframe,
@@ -63,3 +66,72 @@ def train_ulmfit_classifier_with_gradual_unfreezing(learn, lr):
     save_and_load_weights(learn, "unfrozen")
 
     return learn
+
+
+def train_ulmfit(learner):
+    train_ulmfit_classifier_with_gradual_unfreezing(learner, 1e-2)
+    learner.fit_one_cycle(10,
+                          slice((1e-2 / 10) / (2.6 ** 4),
+                                1e-2 / 10),
+                          moms=(0.8, 0.7))
+
+def yield_preds_and_probabilities_from_model(model, dataloader):
+    for xb, yb in dataloader:
+        probabilities = torch.softmax(model(xb)[0], dim=-1).detach().cpu().numpy()
+        preds = np.argmax(probabilities, axis=1)
+        yield preds, probabilities
+
+
+def ulmfit_predict_entire_set(learner, dataloader):
+    predictions, probabilities = zip(*list(yield_preds_and_probabilities_from_model(learner.model,
+                                                                                    dataloader)))
+
+    return (
+        np.concatenate(probabilities, axis=0),
+        np.concatenate(predictions)
+    )
+
+
+def train_ulmfit_model_and_get_validation_and_test_set_predictions(
+    train_dataframe,
+    validation_dataframe,
+    test_dataframe
+):
+    with open('vocab.pkl', 'rb') as vocab_f:
+        vocab = pickle.load(vocab_f)
+
+    learner = load_ulmfit_classifier_with_transfer_learning_from_data_frame(
+        train_dataframe,
+        validation_dataframe,
+        test_dataframe,
+        vocab,
+        'renthop_ulmfit',
+        'fine_tuned_renthop'
+    )
+    train_ulmfit(learner)
+
+    validation_probabilities, validation_preds = ulmfit_predict_entire_set(
+        learner,
+        learner.data.valid_dl
+    )
+    validation_data, validation_labels = tuple(zip(*[
+        x for x in iter(learner.data.valid_dl)
+    ]))
+    validation_labels = torch.cat(validation_labels).cpu().numpy()
+    reconstructed_validation_data = pd.DataFrame(list(itertools.chain.from_iterable([
+        [learner.data.valid_ds.reconstruct(d) for d in validation_batch]
+        for validation_batch in validation_data
+    ])), columns=['description'])
+    report = generate_classification_report_from_preds(validation_preds,
+                                                       validation_probabilities,
+                                                       reconstructed_validation_data,
+                                                       validation_labels,
+                                                       [0, 1, 2],
+                                                       columns=['description'])
+
+    test_probabilities, test_preds = ulmfit_predict_entire_set(
+        learner,
+        learner.data.test_dl
+    )
+
+    return validation_probabilities, test_probabilities
